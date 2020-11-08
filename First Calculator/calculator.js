c0c7896517a22d03a3ec8331ce65bef30a6fdb44
Vladimir Saenko <vova.saenko1@gmail.com>
2020-11-08 13:44:31 +0200

    add js script

--- First Calculator/calculator.js
+++ First Calculator/calculator.js
@@ -1,2 +1,7 @@
 
 "use strict";
+
+var musicbutt = document.getElementById('musicbutt-3');
+musicbutt.addEventListener('click',function () {
+    document.getElementById('sound').play();            
+})
