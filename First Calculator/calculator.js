c20ef8d50f35db8c039f0a5ddef99cd5e5292535
Vladimir Saenko <vova.saenko1@gmail.com>
2020-12-16 23:52:46 +0200

    Update calculator.js

--- First Calculator/calculator.js
+++ First Calculator/calculator.js
@@ -12,10 +12,10 @@ let musicbutt40 = document.getElementById('musicbutt40');
 })
 
 let musicButt0_25 = document.getElementById('alc-1');
-let musicButt0_5 = document.getElementById('alc-2');
 musicButt0_25.addEventListener('click', function() {
     document.getElementById('alcosound').play();
 })
+let musicButt0_5 = document.getElementById('alc-2');
 musicButt0_5.addEventListener('click', function() {
     document.getElementById('alcosound').play();
 })
