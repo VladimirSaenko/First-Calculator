8f6c59c3e225d7cbe5d6b366968a71be9a8fbb8f
Vladimir Saenko <vova.saenko1@gmail.com>
2021-02-23 16:15:58 +0200

    fix

--- calculator.js
+++ calculator.js
@@ -3,7 +3,7 @@
 
 let musicbutt = document.getElementById('musicbutt-3');
 musicbutt.addEventListener('click',function () {
-    document.getElementById('sound').play();
+    document.getElementById('sound-3').play();
 });
 
 let musicbutt40 = document.getElementById('musicbutt40');
